0bcbb1bff3eeb8441cf576ec8aa30c1faa382fca
PICHHI SANTO <156534192+ahmed99000@users.noreply.github.com>
2025-07-30 16:25:18 +0400

    Update Apk.lok.js

--- Apk.lok.js
+++ Apk.lok.js
@@ -1,1 +1,13 @@
-
+{
+  "enable": true,
+  "title": "Login Access",
+  "d1": "Welcome!",
+  "d2": "Please verify to proceed.",
+  "d3": "Secure Access Only",
+  "password": "1234",
+  "highlight": "🔥 Limited Time Access 🔥",
+  "btn_login": "Login",
+  "btn_personal": "Personal",
+  "edit_hint": "Enter password",
+  "personal_link": "https://t.me/sakib01994"
+}
